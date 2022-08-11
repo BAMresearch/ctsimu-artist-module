9d0bdf70673e05909448c6a5901dfaa5f5070c59
David Plotzki <david@tastyorange.de>
2022-08-11 22:17:47 +0200

    ctsimu_main meta-script to source complete package in external scripts.

---
+++ CTSimU/ctsimu_main.tcl
@@ -0,0 +1,10 @@
+# Main CTSimU module file which takes care of sourcing all other files.
+# When using the CTSimU module in your own project,
+# always source this file to get the whole package.
+
+package require TclOO
+package require fileutil
+package require rl_json
+
+variable BasePath [file dirname [info script]]
+source -encoding utf-8 [file join $BasePath ctsimu_scenario.tcl]
